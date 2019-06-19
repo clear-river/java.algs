c9601d9ae00f166e35269d2748ef55a8eeffdb9d
Administrator <Administrator@chenyang-Aux>
2019-06-19 22:53:01 +0800

    save TireCheck first. To debug.

---
+++ src/main/java/chenyang/dynamic/TireCheck.java
@@ -0,0 +1,96 @@
+package chenyang.dynamic;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class TireCheck {
+	//使用位运算，根据已经包含的步骤计算一个key。这样所有包含相同步骤的plans（尽管排序不同）将会
+	//得到相同的key。使用这个key来map所有这些plan中max_pressure最大的可行plan。
+	private HashMap<Long, CheckPlan> plan_dict;
+	private Queue<CheckPlan> plan_part;
+	private PlanStep[] available_steps;
+	
+	private class CheckPlan{
+		private LinkedList<Integer> steps;
+		private Long plan_key = 0L;
+		private int max_pressure = 0;
+//		TODO: private int min_pressure = 0;
+		private int result_pressure = 0;
+		
+		public CheckPlan add_step(int step_index) {
+			if (steps.contains(step_index)) {
+				return null;
+			}
+			
+			if (result_pressure + available_steps[step_index].pres_inc > 100) {
+				return null;
+			}
+			
+			if (result_pressure + available_steps[step_index].pres_inc - 
+					available_steps[step_index].pres_dec < 0) {
+				return null;
+			}
+			
+			
+			CheckPlan new_plan = new CheckPlan();
+			new_plan.steps = new LinkedList<Integer>();
+			new_plan.steps.addAll(steps);
+			new_plan.steps.add(step_index);
+			
+			new_plan.plan_key = plan_key + (1 << step_index);
+			
+			if (result_pressure + available_steps[step_index].pres_inc > max_pressure) {
+				new_plan.max_pressure = result_pressure + available_steps[step_index].pres_inc;
+			}
+			
+			new_plan.result_pressure = result_pressure + available_steps[step_index].pres_inc -
+					available_steps[step_index].pres_dec;
+			
+			return new_plan;
+		}
+	}
+	
+	public void init() {
+		plan_dict = new HashMap<Long, CheckPlan>();
+		plan_part = new Queue<CheckPlan>();
+		available_steps = new PlanStep[8];
+		//TODO: fill up test data...
+	}
+	
+	public CheckPlan create_plan() {
+		CheckPlan plan = new CheckPlan();
+		plan.steps = new LinkedList<Integer>();
+		plan_part.add(plan);
+		
+		while(!plan_part.isEmpty()) {
+			plan = plan_part.poll();
+			for (int i = 0; i < available_steps.length; i++) {
+				CheckPlan next_plan = plan.add_step(i);
+				if (next_plan == null) { continue; }
+				
+				if (plan_dict.containsKey(next_plan.plan_key)) {
+					CheckPlan known_plan = plan_dict.get(next_plan.plan_key);
+					if (next_plan.max_pressure > known_plan.max_pressure) {
+						plan_dict.put(next_plan.plan_key, next_plan);
+					}
+				}else {
+					plan_dict.put(next_plan.plan_key, next_plan);
+				}
+				
+				if (next_plan.steps.size() != available_steps.length) {
+					plan_part.add(next_plan);
+				}
+			}
+			plan_dict.remove(plan.plan_key);
+		}
+		
+		assert(plan_dict.size() == 1);
+		return (CheckPlan) plan_dict.values().toArray()[0];
+	}	
+}
+
+class PlanStep{
+	int pres_inc;
+	int pres_dec;
+}
