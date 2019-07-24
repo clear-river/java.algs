f444ec6d8a229849d4ea5343a7c879ef78589b00
clear-river <yangchen_buaasei@hotmail.com>
2019-07-24 17:17:09 +0800

    add an auxiliary class for SCC detection of DIGraph.

---
+++ src/main/java/chenyang/graph/DepthFirstOrder.java
@@ -0,0 +1,12 @@
+//java DepthFirstOrder tinyDAG.txt
+package chenyang.graph;
+
+import java.util.Queue;
+
+public class DepthFirstOrder {
+	private boolean[] marked;
+	private Queue<Integer> preOrder;
+	private Queue<Integer> postOrder;
+	
+	
+}
