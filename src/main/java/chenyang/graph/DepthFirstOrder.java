e52e2447e709a6b51c1f28a18404285bb788d309
clear-river <yangchen_buaasei@hotmail.com>
2019-07-24 17:25:39 +0800

    update dfo.

--- src/main/java/chenyang/graph/DepthFirstOrder.java
+++ src/main/java/chenyang/graph/DepthFirstOrder.java
@@ -1,6 +1,7 @@
 //java DepthFirstOrder tinyDAG.txt
 package chenyang.graph;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class DepthFirstOrder {
@@ -8,5 +9,24 @@ public class DepthFirstOrder {
 	private Queue<Integer> preOrder;
 	private Queue<Integer> postOrder;
 	
+	public DepthFirstOrder(Digraph g) {
+		marked = new boolean[g.V()];
+		preOrder = new LinkedList<Integer>();
+		postOrder = new LinkedList<Integer>();
+		
+		for (int v = 0; v < g.V(); v++) {
+			if (marked[v]) { continue; }
+			depthFirstSearch(g, v);
+		}
+	}
 	
+	private void depthFirstSearch(Digraph g, int v) {
+		marked[v] = true;
+		preOrder.add(v);
+		for (int w : g.adj(v)) {
+			if (marked[w]) { continue; }
+			depthFirstSearch(g, w);
+		}
+		postOrder.add(v);
+	}
 }
