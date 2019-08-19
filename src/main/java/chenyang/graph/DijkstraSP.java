5595a8f383fa6778958283972e89217f98e091e9
clear-river <yangchen_buaasei@hotmail.com>
2019-08-19 17:12:30 +0800

    Traced Dijkstra Shortest Path.

---
+++ src/main/java/chenyang/graph/DijkstraSP.java
@@ -0,0 +1,92 @@
+package chenyang.graph;
+
+import java.util.LinkedList;
+import chenyang.auxiliary.In;
+import chenyang.auxiliary.IndexMinPQ;
+import chenyang.auxiliary.StdOut;
+
+public class DijkstraSP {
+    private double[] distTo;          // distTo[v] = distance  of shortest s->v path
+    private DirectedEdge[] edgeTo;    // edgeTo[v] = last edge on shortest s->v path
+    private IndexMinPQ<Double> pq;    // priority queue of vertices
+    
+    public DijkstraSP(EdgeWeightedDigraph G, int s) {
+        for (DirectedEdge e : G.edges()) {
+            if (e.weight() < 0)
+                throw new IllegalArgumentException("edge " + e + " has negative weight");
+        }
+
+        distTo = new double[G.V()];
+        edgeTo = new DirectedEdge[G.V()];
+
+        for (int v = 0; v < G.V(); v++)
+            distTo[v] = Double.POSITIVE_INFINITY;
+        distTo[s] = 0.0;
+
+        // relax vertices in order of distance from s
+        pq = new IndexMinPQ<Double>(G.V());
+        pq.insert(s, distTo[s]);
+        while (!pq.isEmpty()) {
+            int v = pq.delMin();
+            relax(G, v);
+        }
+    }
+    
+    private void relax(EdgeWeightedDigraph G, int v) {
+        for (DirectedEdge e : G.adj(v)) { relax(e); }
+    }
+    
+    private void relax(DirectedEdge e) {
+        int v = e.from(), w = e.to();
+        if (distTo[w] > distTo[v] + e.weight()) {
+            distTo[w] = distTo[v] + e.weight();
+            edgeTo[w] = e;
+            if (pq.contains(w)) { pq.decreaseKey(w, distTo[w]); }
+            else { pq.insert(w, distTo[w]); }
+        }
+    }
+    
+    public double distTo(int v) {
+        return distTo[v];
+    }
+
+    public boolean hasPathTo(int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+
+    public Iterable<DirectedEdge> pathTo(int v) {
+        if (!hasPathTo(v)) return null;
+        LinkedList<DirectedEdge> path = new LinkedList<DirectedEdge>();
+        for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
+            path.addFirst(e);
+        }
+        return path;
+    }
+    
+    public static void main(String[] args) {
+        In in = new In(args[0]);
+        EdgeWeightedDigraph G = new EdgeWeightedDigraph(in);
+        StdOut.println(G);
+        
+        
+        int s = Integer.parseInt(args[1]);
+
+        // compute shortest paths
+        DijkstraSP sp = new DijkstraSP(G, s);
+
+
+        // print shortest path
+        for (int t = 0; t < G.V(); t++) {
+            if (sp.hasPathTo(t)) {
+                StdOut.printf("%d to %d (%.2f)  ", s, t, sp.distTo(t));
+                for (DirectedEdge e : sp.pathTo(t)) {
+                    StdOut.print(e + "   ");
+                }
+                StdOut.println();
+            }
+            else {
+                StdOut.printf("%d to %d         no path\n", s, t);
+            }
+        }
+    }
+}
